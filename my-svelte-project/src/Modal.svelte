dbbb13e48a9d3ffaff68eb5d35f97572f21d7680
AlaaElattar <alaamahmoud.1223@gmail.com>
2022-07-29 00:20:57 +0200

    FE

--- my-svelte-project/src/Modal.svelte
+++ my-svelte-project/src/Modal.svelte
@@ -31,13 +31,14 @@ async function createTask(){
         <div id="myDIV" class="header">
             <h2>My To Do List</h2>
             <input type="text" id="myInput" placeholder="Title...">
-            <button type="button" on:click={createTask}> Add</button>
+            <button id="Button" type="button" on:click={createTask}> Add</button>
           </div>
           
           <ul id="myUL">
            
             {#each lists as list }
-          <li > {list.task}</li> 
+            <input type="checkbox" id="checkBox"> <li > {list.task}</li> <span id="span">❌</span>
+          
             {/each}
           </ul>
     </dir>
@@ -64,20 +65,20 @@ ul {
 }
 
 /* Style the list items */
-ul li {
+/* ul li {
   cursor: pointer;
   position: relative;
   padding: 12px 8px 12px 40px;
   background: #eee;
   font-size: 18px;
-  transition: 0.2s;
+  transition: 0.2s; */ 
 
-  /* make the list items unselectable */
+  /* make the list items unselectable
   -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
-  user-select: none;
-}
+  user-select: none; */
+/*}*/
 
 /* Set all odd list items to a different color (zebra-stripes) */
 ul li:nth-child(odd) {
@@ -90,14 +91,14 @@ ul li:hover {
 }
 
 /* When clicked on, add a background color and strike out text */
-ul li.checked {
+/* ul li.checked {
   background: #888;
   color: #fff;
   text-decoration: line-through;
-}
+} */
 
 /* Add a "checked" mark when clicked on */
-ul li.checked::before {
+/* ul li.checked::before {
   content: '';
   position: absolute;
   border-color: #fff;
@@ -108,7 +109,7 @@ ul li.checked::before {
   transform: rotate(45deg);
   height: 15px;
   width: 7px;
-}
+} */
 
 /* Style the close button */
 .close {
@@ -122,6 +123,7 @@ ul li.checked::before {
   background-color: #f44336;
   color: white;
 }
+
 
 /* Style the header */
 .header {
@@ -163,8 +165,31 @@ input {
   border-radius: 0;
 }
 
-.addBtn:hover {
-  background-color: #bbb;
-}
+li {
+    list-style: none;
+    padding: 10px 10px;
+    border-bottom: 2px solid rgb(105, 67, 67);
+  }
+
+  #checkBox{
+    position: absolute;
+    margin-top: 12px;
+  }
+
+  #span{
+    position: absolute;
+    left: 100px;
+    margin-top: -25px;
+  }
+
+  #Button{
+    display: inline-block;
+    font-size: 20px;
+    border-radius: 0px;
+    background-color: white;
+    width: 18%;
+    
+
+  }
 
 </style>
