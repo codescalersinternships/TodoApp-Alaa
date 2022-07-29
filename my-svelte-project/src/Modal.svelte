a8b9a43b2a937689bb102bd1a031b8c0d25f3437
AlaaElattar <alaamahmoud.1223@gmail.com>
2022-07-29 09:27:13 +0200

    FE

--- my-svelte-project/src/Modal.svelte
+++ my-svelte-project/src/Modal.svelte
@@ -12,7 +12,7 @@ async function createTask(){
   const res = await fetch('http://localhost:8080/todo`',{
     method: 'POST',
     body: JSON.stringify({
-      id : 4,
+      id,
       task,
       done: false
 
@@ -21,6 +21,10 @@ async function createTask(){
 
   const json = await res.json()
   result = JSON.stringify(json)
+  lists.push(task)
+  var node = document.createElement('li');
+  node.appendChild(document.createTextNode('myUL'));
+  document.querySelector('ul').appendChild(node);
 }
 
 
@@ -31,16 +35,16 @@ async function createTask(){
         <div id="myDIV" class="header">
             <h2>My To Do List</h2>
             <input type="text" id="myInput" placeholder="Title...">
-            <button id="Button" type="button" on:click={createTask}> Add</button>
+            <button id="Button" type="button" onclick="createTask()"> Add</button>
           </div>
           
-          <ul id="myUL">
+          <ol id="myUL">
            
             {#each lists as list }
-            <input type="checkbox" id="checkBox"> <li > {list.task}</li> <span id="span">❌</span>
+            <input type="checkbox" id="checkBox"> <li >{list.id} {list.task}</li> <span id="span">❌</span>
           
             {/each}
-          </ul>
+          </ol>
     </dir>
 
     <section>
@@ -53,34 +57,16 @@ async function createTask(){
 
 
 <style>
-    	/* Include the padding and border in an element's total width and height */
+    
 * {
   box-sizing: border-box;
 }
 
-/* Remove margins and padding from the list */
 ul {
   margin: 0;
   padding: 0;
 }
 
-/* Style the list items */
-/* ul li {
-  cursor: pointer;
-  position: relative;
-  padding: 12px 8px 12px 40px;
-  background: #eee;
-  font-size: 18px;
-  transition: 0.2s; */ 
-
-  /* make the list items unselectable
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none; */
-/*}*/
-
-/* Set all odd list items to a different color (zebra-stripes) */
 ul li:nth-child(odd) {
   background: #f9f9f9;
 }
@@ -90,28 +76,6 @@ ul li:hover {
   background: #ddd;
 }
 
-/* When clicked on, add a background color and strike out text */
-/* ul li.checked {
-  background: #888;
-  color: #fff;
-  text-decoration: line-through;
-} */
-
-/* Add a "checked" mark when clicked on */
-/* ul li.checked::before {
-  content: '';
-  position: absolute;
-  border-color: #fff;
-  border-style: solid;
-  border-width: 0 2px 2px 0;
-  top: 10px;
-  left: 16px;
-  transform: rotate(45deg);
-  height: 15px;
-  width: 7px;
-} */
-
-/* Style the close button */
 .close {
   position: absolute;
   right: 0;
@@ -124,8 +88,6 @@ ul li:hover {
   color: white;
 }
 
-
-/* Style the header */
 .header {
   background-color: #ca8c87;
   padding: 30px 40px;
@@ -133,14 +95,12 @@ ul li:hover {
   text-align: center;
 }
 
-/* Clear floats after the header */
 .header:after {
   content: "";
   display: table;
   clear: both;
 }
 
-/* Style the input */
 input {
   margin: 0;
   border: none;
@@ -151,7 +111,6 @@ input {
   font-size: 16px;
 }
 
-/* Style the "Add" button */
 .addBtn {
   padding: 10px;
   width: 25%;
